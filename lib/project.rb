3a08110b20f2daa6144b32d8682e27a81b2abe08
Carl J <carlj1881@yahoo.com>
2020-10-07 01:21:07 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+
+class Project
+  
+  attr_reader :title, :backers
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def self.backers
+    @backers
+  end
+  
+  def add_backer(self)
+    @backers << self
+  end
+  
+end
